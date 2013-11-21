f8afca08158afb48bd5fcb5b6e228d496bb5d479
Klederson Bueno <klederson@klederson.com>
2013-11-21 00:40:32 -0200

    Update RestServer.php

--- restnpeace/material/RestServer.php
+++ restnpeace/material/RestServer.php
@@ -50,6 +50,7 @@ abstract class RestServer extends Controller {
         $this->body = RestTools::parseBody($_GET, 'array');
         break;
       default:
+        $raw = empty(file_get_contents('php://input')) ? $_REQUEST : file_get_contents('php://input'); //now support strict form-data and x-www-form-urlencoded
         $this->body = RestTools::parseBody(file_get_contents('php://input'), $this->getInputFormat());
         break;
     }
@@ -59,11 +60,39 @@ abstract class RestServer extends Controller {
   }
 
   /**
+   * In order to have better dynamic funciton based on the method now we define the function
+   * by putting their methods before the function's name, ex.: get_user, post_user, delete_user
+   * and than we can call the different functions just by changing it's methods types.
+   */
+  public function __call($name, $arguments) {
+    $methodName = sprintf("%s_%s",$name,$this->getMethod());
+    $generic = sprintf("%s_generic",$name);
+
+    if($this->getMethod() == "options") {
+      $this->sendResponse(201,["message" => "PhpBURN"]);
+    }
+
+    if(method_exists($this, $methodName)) {
+      return call_user_func_array([$this,$methodName], $arguments);
+    }
+
+    if(method_exists($this, $generic)) {
+      return call_user_func_array([$this,$generic], $arguments);
+    }
+
+    $response['status'] = self::ERROR;
+    $response['messages']['error'] = PhpBURN_Views::lazyTranslate('[!This method does not exists or cannot accept the given method!]: ' . $name . " : " . $this->getMethod());
+    $httpStatus = 404;
+
+    $this->sendResponse($httpStatus, $response, []);
+  }
+
+  /**
    * This retreives authentication data sent
    * 
    * @param Array $params
    * @param String $type
-   * @return Array 
+   * @return Array
    */
   public function getAuthData(array $params = array(), $type = 'basic') {
     //For now we support only BASIC AUTHENTICATION
@@ -76,7 +105,7 @@ abstract class RestServer extends Controller {
   public function sendResponse($status, array $content = array(), array $headers = array(), $contentType = "application/json") {
     $headers = array_merge($this->responseHeaders, $headers);
 
-    $headers['X-Powered-by'] = "iFind Platform";
+    $headers['X-Powered-by'] = "PhpBURN Framework";
     $headers['Access-Control-Allow-Origin'] = isset($_SERVER['HTTP_ORIGIN']) ? $_SERVER['HTTP_ORIGIN'] : "*";
     $headers['Access-Control-Allow-Headers'] = "origin, contenttype, content-type, accept, x-requested-with";
     $headers['Access-Control-Allow-Credentials'] = "true";
@@ -100,12 +129,15 @@ abstract class RestServer extends Controller {
     if (count($this->allowedContentTypes) == 0)
       return TRUE;
 
+    //PHP 5.4 introduces HTTP_CONTENT_TYPE instead CONTENT_TYPE
+    $requestType = empty($_SERVER['HTTP_CONTENT_TYPE']) ? $_SERVER['CONTENT_TYPE'] : $_SERVER['HTTP_CONTENT_TYPE'];
+
     foreach ($this->allowedContentTypes as $contentType => $format) {
-      if (preg_match("($contentType)", $_SERVER['CONTENT_TYPE'])) {
+      if (preg_match("($contentType)", $requestType)) {
         $this->contentType = $contentType;
         $this->inputFormat = $format;
         return TRUE;
-      } else if($_SERVER['CONTENT_TYPE'] == NULL) {
+      } else if($requestType === NULL) {
         $this->contentType = $defaultContentType;
         $this->inputFormat = $defaultFormat;
 
